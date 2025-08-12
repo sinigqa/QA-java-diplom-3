00d3cd0b4f99aa2d9bab0e8fecde8714d4aca5fb
Igor Pavlov <sinigamimasters@gmail.com>
2025-08-12 17:11:10 +0300

    выбрал метод запуска браузера через системную переменную (-D)

--- src/main/java/org/example/config/BaseClass.java
+++ src/main/java/org/example/config/BaseClass.java
@@ -5,42 +5,47 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
-
 import java.io.File;
 
-
 public class BaseClass {
-    public static WebDriver createWebDriver(String browser) {
+    public static WebDriver createWebDriver() {
+        String browser = System.getProperty("browser", "chrome"); // "chrome" по умолчанию
+        return createDriver(browser);
+    }
+
+    private static WebDriver createDriver(String browser) {
         switch (browser.toLowerCase()) {
-            case "chrome":
-                WebDriverManager.chromedriver().setup();
-                return new ChromeDriver();
-
             case "yandex":
-                File driverExecutable = new File("C:/Users/user/chromedriver-win64/chromedriver.exe");
-
-                ChromeDriverService service = new ChromeDriverService.Builder()
-                        .usingDriverExecutable(driverExecutable)
-                        .build();
-
-                ChromeOptions options = new ChromeOptions();
-                options.setBinary("C:/Users/user/AppData/Local/Yandex/YandexBrowser/Application/browser.exe");
-
-                options.addArguments(
-                        "--remote-allow-origins=*",
-                        "--no-sandbox",
-                        "--disable-dev-shm-usage",
-                        "--disable-blink-features=AutomationControlled",
-                        "--start-maximized",
-                        "--disable-extensions"
-                );
-
-                options.setExperimentalOption("detach", true);
-
-                return new ChromeDriver(service, options);
-
+                return setupYandexDriver();
+            case "chrome":
             default:
-                throw new IllegalArgumentException("Unsupported browser: " + browser);
+                return setupChromeDriver();
         }
+    }
+
+    private static WebDriver setupChromeDriver() {
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver();
+    }
+
+    private static WebDriver setupYandexDriver() {
+        File driverExecutable = new File("C:/Users/user/chromedriver-win64/chromedriver.exe");
+        ChromeDriverService service = new ChromeDriverService.Builder()
+                .usingDriverExecutable(driverExecutable)
+                .build();
+
+        ChromeOptions options = new ChromeOptions();
+        options.setBinary("C:/Users/user/AppData/Local/Yandex/YandexBrowser/Application/browser.exe");
+        options.addArguments(
+                "--remote-allow-origins=*",
+                "--no-sandbox",
+                "--disable-dev-shm-usage",
+                "--disable-blink-features=AutomationControlled",
+                "--start-maximized",
+                "--disable-extensions"
+        );
+        options.setExperimentalOption("detach", true);
+
+        return new ChromeDriver(service, options);
     }
 }
--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,32 +1,18 @@
-import org.junit.Before;
 import org.example.config.BaseClass;
 import org.junit.After;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+import org.junit.Before;
 import org.openqa.selenium.WebDriver;
-
 import java.time.Duration;
 
-@RunWith(Parameterized.class)
 public abstract class BaseTest {
     protected WebDriver driver;
 
-
-    @Parameterized.Parameter
-    public String browser;
-
-
-    @Parameterized.Parameters(name = "Browser: {0}")
-    public static String[] browsers() {
-        return new String[]{"chrome", "yandex"};
-    }
-
     @Before
     public void setUp() {
-
-            driver = BaseClass.createWebDriver(browser);
-            driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        // Теперь метод createWebDriver() без параметров
+        driver = BaseClass.createWebDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     }
 
     @After
