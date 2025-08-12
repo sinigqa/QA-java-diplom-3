a4b6949a16cfc0b34ce724bc980d523fce6a3c27
Igor Pavlov <sinigamimasters@gmail.com>
2025-08-12 17:50:01 +0300

    Убрал лишнее из MainPage добавил метод для проверки активной вкладки

--- src/main/java/org/example/pages/MainPage.java
+++ src/main/java/org/example/pages/MainPage.java
@@ -2,6 +2,7 @@ package org.example.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
@@ -14,10 +15,6 @@ public class MainPage {
     private final By bunsTab = By.xpath("//div[contains(@class, 'tab_tab__1SPyG')]//span[text()='Булки']/..");
     private final By saucesTab = By.xpath("//div[contains(@class, 'tab_tab__1SPyG')]//span[text()='Соусы']/..");
     private final By fillingsTab = By.xpath("//div[contains(@class, 'tab_tab__1SPyG')]//span[text()='Начинки']/..");
-
-    private final By bunsImage = By.xpath("//img[@src='https://code.s3.yandex.net/react/code/bun-01.png']");
-    private final By saucesImage = By.xpath("//img[@src='https://code.s3.yandex.net/react/code/sauce-01.png']");
-    private final By fillingsImage = By.xpath("//img[@src='https://code.s3.yandex.net/react/code/meat-03.png']");
 
     private final By loginButton = By.xpath("//button[text()='Войти в аккаунт']");
     private final By personalAccountLink = By.xpath("//p[text()='Личный Кабинет']");
@@ -43,23 +40,28 @@ public class MainPage {
         wait.until(ExpectedConditions.elementToBeClickable(fillingsTab)).click();
     }
 
-    public boolean isBunsSectionDisplayed() {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(bunsImage)).isDisplayed();
-    }
-
-    public boolean isSaucesSectionDisplayed() {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(saucesImage)).isDisplayed();
-    }
-
-    public boolean isFillingsSectionDisplayed() {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(fillingsImage)).isDisplayed();
-    }
-
     public void clickLoginButton() {
         wait.until(ExpectedConditions.elementToBeClickable(loginButton)).click();
     }
 
     public void clickPersonalAccount() {
         wait.until(ExpectedConditions.elementToBeClickable(personalAccountLink)).click();
+    }
+
+    public boolean isTabActive(By tabLocator) {
+        WebElement tab = wait.until(ExpectedConditions.presenceOfElementLocated(tabLocator));
+        return tab.getAttribute("class").contains("tab_tab_type_current__2BEPc");
+    }
+
+    public boolean isBunsTabActive() {
+        return isTabActive(bunsTab);
+    }
+
+    public boolean isSaucesTabActive() {
+        return isTabActive(saucesTab);
+    }
+
+    public boolean isFillingsTabActive() {
+        return isTabActive(fillingsTab);
     }
 }
